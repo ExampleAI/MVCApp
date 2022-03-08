517b14cf87dc022debabf005a1a7505a5f9c3592
paulmcpg <84036679+paulmcpg@users.noreply.github.com>
2022-03-08 08:53:33 -0500

    Add project files.

---
+++ mvcX/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace myNamespace.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
